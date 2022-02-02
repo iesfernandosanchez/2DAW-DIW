ab7819774a2e27871f7ac6192faf286d451bb9dc
Fernando Sanchez <ies.fernandosanchez@gmail.com>
2022-02-02 18:11:31 +0100

    02/02/2022 actualizando repositorio con tareas no subidas

---
+++ site/web/2DAW-DIW-Carlos.Bouzas/2ºEvaluacion/Inventario/js/productos.js
@@ -0,0 +1,10 @@
+var productos = [{
+    'name': 'tomate',
+    'precio': '1.23',
+    'imagen': 'default.jpg'
+},
+    {
+        'name':'pepino',
+        'precio':'2.31',
+        'imagen':'pepino.jpg'
+    }]
