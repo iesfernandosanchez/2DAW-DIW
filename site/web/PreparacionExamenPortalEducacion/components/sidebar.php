2914110d628734e710ab02c3c9d3fc37fb49d442
Jose Manuel de Dios Martin <jm.dios.martin@gmail.com>
2022-02-11 15:38:37 +0100

    Subida

---
+++ site/web/PreparacionExamenPortalEducacion/components/sidebar.php
@@ -0,0 +1,55 @@
+    <!-- ======= Sidebar ======= -->
+    <aside id="sidebar" class="sidebar">
+        
+        <ul class="sidebar-nav" id="sidebar-nav">
+            
+            <!-- Cursos Nav -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                    <i class="bi bi-grid"></i>
+                    <span>Cursos</span>
+                </a>
+            </li><!-- End Cursos Nav -->
+
+            <!-- Alumnos Nav -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                    <i class="bi bi-grid"></i>
+                    <span>Alumnos</span>
+                </a>
+            </li><!-- End Alumnos Nav -->
+            
+            <!-- Notas Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                    <i class="bi bi-menu-button-wide"></i><span>Notas</span><i class="bi bi-chevron-down ms-auto"></i>
+                </a>
+                <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                    <li>
+                        <a href="components-alerts.html">
+                            <i class="bi bi-circle"></i><span>1ª Evaluación</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="components-alerts.html">
+                            <i class="bi bi-circle"></i><span>2ª Evaluación</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="components-alerts.html">
+                            <i class="bi bi-circle"></i><span>3ª Evaluación</span>
+                        </a>
+                    </li>
+                </ul>
+            </li><!-- End Notas Nav -->
+
+            <!-- Incidencias Nav -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                    <i class="bi bi-grid"></i>
+                    <span>Incidencias</span>
+                </a>
+            </li><!-- End Incidencias Nav -->
+        </ul>
+        
+    </aside><!-- End Sidebar-->
